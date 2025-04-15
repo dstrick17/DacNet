4034f250b878aa1a3d5d642e749e84735448c566
anthua20 <tsehou26@bu.edu>
2025-04-15 16:14:29 -0400

    transform testing

--- scripts/aux_chexnet.py
+++ scripts/aux_chexnet.py
@@ -30,10 +30,12 @@ CONFIG = {
     "image_size": 224,  # Consistent image size
 }
  
-# Define image transformations (consistent with CheXNet)
+# Define image transformations
 transform_train = transforms.Compose([
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),  # ImageNet normalization
 ])
