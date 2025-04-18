3fd49a31c4ae5c7270414c082ecc29cc27a2f4b8
anthua20 <tsehou26@bu.edu>
2025-04-18 15:14:34 -0400

    edit for test

--- scripts/densenet121.py
+++ scripts/densenet121.py
@@ -38,8 +38,6 @@ CONFIG = {
 transform_train = transforms.Compose([
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(15),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),  # ImageNet normalization
 ])
